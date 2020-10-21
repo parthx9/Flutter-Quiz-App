d68b2492977567edc0d737d7f97f9270fd535848
ParthArora <partharora.india@gmail.com>
2020-10-21 15:53:33 +0530

    Quiz App Setup

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,32 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What's Your Favourite color?",
+      "Whats Your Favourite Animal"
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My First App"),
         ),
-        body: Text("This is my defualt text!"),
+        body: Column(
+          children: <Widget>[
+            Text("The question"),
+            RaisedButton(
+              child: Text("Answer 1"),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text("Answer 2"),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text("Answer 3"),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
