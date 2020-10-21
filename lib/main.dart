11c8a6b6e681e57b7cf85e935ed25edd3b710488
ParthArora <partharora.india@gmail.com>
2020-10-21 15:08:35 +0530

    Initial Setup

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text("Hello"),
+    );
+  }
+}
